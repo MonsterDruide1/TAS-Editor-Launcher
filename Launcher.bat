fce6de98b5ad61d902019d02ba8717922b300ca9
MonsterDruide1 <5958456@gmail.com>
2021-06-03 10:57:48 +0200

    Update Launcher.bat

--- Launcher.bat
+++ Launcher.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-java -jar Launcher.jar & if ERRORLEVEL 3 call Launcher-updater.bat
+start "TAS-Editor-Launcher" /MIN cmd /c "java -jar Launcher.jar v0 & if ERRORLEVEL 3 call Launcher-updater.bat"
