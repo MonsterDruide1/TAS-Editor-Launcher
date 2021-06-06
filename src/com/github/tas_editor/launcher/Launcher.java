a441359422ede150ee647e0f5b57196ff409e068
MonsterDruide1 <5958456@gmail.com>
2021-06-06 17:34:32 +0200

    Update Launcher.java

--- src/com/github/tas_editor/launcher/Launcher.java
+++ src/com/github/tas_editor/launcher/Launcher.java
@@ -46,7 +46,7 @@ public class Launcher {
 			Preferences.userRoot().node(Launcher.class.getName()).clear();
 		
 		prefs = Preferences.userRoot().node(Launcher.class.getName());
-		if(args.length == 0) { //first start or just didn't start using the bat
+		if(args.length == 0) { //first start or just didn't start using the bat 
 			File ownFile = new File(Launcher.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
 			System.out.println(ownFile+" -> "+ownFile.getParent());
 			if(!prefs.getBoolean("installed", false) && !prefs.getBoolean("justInstalled", false) && !ownFile.getParent().equals("bin")) { //first start -> install
